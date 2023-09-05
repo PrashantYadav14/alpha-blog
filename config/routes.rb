d8ebd4cc4060ec2f84e40de60ad710175a693189
PrashantYadav14 <prashantyadav.sjs@gmail.com>
2023-09-05 21:30:42 +0530

    Final

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,4 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   resources :categories, except: [:destroy]
-
-  get 'search', to: 'search#search_user'
 end
